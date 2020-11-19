869dbb20da24d2757edb4fc700a37c0260698e38
djlim98 <ehdwlsdudwo1@gmail.com>
2020-11-19 13:57:05 +0900

    youtube, naver mobile 추가

--- main.py
+++ main.py
@@ -28,5 +28,37 @@ def naver_scraping():
 
     threading.Timer(5, naver_scraping).start()
 
-naver_scraping()
+def mobile_naver_scraping():
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get('https://m.naver.com/')
+    time.sleep(3)
+    
+    mobile_small_img=driver.find_element_by_class_name('ad_banner_img').get_attribute('src')
+    print(mobile_small_img)
+    req = urllib.request.urlretrieve(mobile_small_img, "mobile_small_img_" + time.strftime('%m-%d-%H', time.localtime(time.time())) + ".jpg")
+    
+    driver.quit()
+
+    threading.Timer(5, mobile_naver_scraping).start()
+
+def youtube_scraping():
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get('https://www.youtube.com/')
+    time.sleep(3)
+    
+    ad_link=driver.find_element_by_xpath('//*[@id="click-target"]').get_attribute('href')
+    print(ad_link)
+    
+    driver.get(ad_link)
+    time.sleep(3)
+    ad_subject=driver.find_element_by_xpath('//*[@id="container"]/h1/yt-formatted-string').text
+    print(ad_subject)
+    f=open("youtube_ad_list.txt",'a', encoding='utf8')
+    f.writelines(time.strftime('%m-%d-%H', time.localtime(time.time()))+' '+ad_subject+' LINK : '+ad_link+'\n')
+    f.close()
+    driver.quit()
+
+    threading.Timer(5, youtube_scraping).start()
+
+youtube_scraping()
                         
