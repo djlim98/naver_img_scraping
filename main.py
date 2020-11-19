ff92cbce5db24d7dd94a2a8ce6f7300e1cf5c930
djlim98 <ehdwlsdudwo1@gmail.com>
2020-11-19 14:21:05 +0900

    이미지 path 수정

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ def naver_scraping():
     driver.get('https://www.naver.com/')
     time.sleep(3)
 
-    small_imgs=driver.find_elements_by_xpath('//*[@id="da_brand"]/div/a/div/img')
+    small_imgs=driver.find_elements_by_xpath('//*[@id="da_brand"]//img')
     counter=0
     for i in small_imgs:
         counter+=1
         s1=i.get_attribute('src')
         print(i.get_attribute('src'))
-        req = urllib.request.urlretrieve(s1, "small_img_"+str(counter) + time.strftime('%m-%d-%H', time.localtime(time.time())) + ".jpg")
+        req = urllib.request.urlretrieve(s1, "small_img_"+str(counter) + time.strftime('%m-%d-%I%p-%M-%S', time.localtime(time.time())) + ".jpg")
 
     driver.switch_to.frame('da_iframe_time')
     big_img = driver.find_element_by_xpath('//*[@id="ac_banner_a"]/img').get_attribute('src')
     print(big_img)
-    req = urllib.request.urlretrieve(big_img, "big_img_" + time.strftime('%m-%d-%H', time.localtime(time.time())) + ".jpg")
+    req = urllib.request.urlretrieve(big_img, "big_img_" + time.strftime('%m-%d-%I%p-%M-%S', time.localtime(time.time())) + ".jpg")
 
     driver.quit()
 
@@ -33,9 +33,13 @@ def mobile_naver_scraping():
     driver.get('https://m.naver.com/')
     time.sleep(3)
     
-    mobile_small_img=driver.find_element_by_class_name('ad_banner_img').get_attribute('src')
+    mobile_small_img=driver.find_elements_by_xpath('//*[@id="main_search_specialda_1"]//img')
     print(mobile_small_img)
-    req = urllib.request.urlretrieve(mobile_small_img, "mobile_small_img_" + time.strftime('%m-%d-%H', time.localtime(time.time())) + ".jpg")
+    seq=0
+    for i in mobile_small_img:
+        seq=seq+1
+        mobile_small_img_link=i.get_attribute('src')
+        req = urllib.request.urlretrieve(mobile_small_img_link, "mobile_small_img_"+str(seq)+"_"+ time.strftime('%m-%d-%I%p-%M-%S', time.localtime(time.time())) + ".jpg")
     
     driver.quit()
 
@@ -54,11 +58,11 @@ def youtube_scraping():
     ad_subject=driver.find_element_by_xpath('//*[@id="container"]/h1/yt-formatted-string').text
     print(ad_subject)
     f=open("youtube_ad_list.txt",'a', encoding='utf8')
-    f.writelines(time.strftime('%m-%d-%H', time.localtime(time.time()))+' '+ad_subject+' LINK : '+ad_link+'\n')
+    f.writelines(time.strftime('%m-%d-%I%p-%M-%S', time.localtime(time.time()))+' '+ad_subject+' LINK : '+ad_link+'\n')
     f.close()
     driver.quit()
 
     threading.Timer(5, youtube_scraping).start()
 
-youtube_scraping()
+mobile_naver_scraping()
                         
