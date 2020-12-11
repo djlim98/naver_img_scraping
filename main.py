04a83eb611984e2fe3c07a999593b2b1158211d5
djlim98 <ehdwlsdudwo1@gmail.com>
2020-12-11 10:19:39 +0900

    폴더 생성 및 저장, youtube 수정중

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def naver_scraping():
     time.sleep(3)
 
     small_imgs=driver.find_elements_by_xpath('//*[@id="da_brand"]//img')
-    counter=0
+    seq=0
     for i in small_imgs:
-        counter+=1
+        seq+=1
         s1=i.get_attribute('src')
         print(i.get_attribute('src'))
-        req = urllib.request.urlretrieve(s1, "small_img_"+str(counter) +'_'+ time.strftime('%m-%d-%I%p-%M-%S', time.localtime(time.time())) + ".jpg")
+        req = urllib.request.urlretrieve(s1, time.strftime('%Y-%m-%d/%Y-%m-%d_naver_%H시_small', time.localtime(time.time()))+str(seq) + ".jpg")
 
     
     big_imgs = driver.find_elements_by_xpath('//*[@id="da_top"]//img')
@@ -26,12 +26,12 @@ def naver_scraping():
         driver.switch_to.frame('da_iframe_time')
         big_imgs = driver.find_elements_by_xpath('//*[@id="da_timeboard"]//img')
     print(big_imgs)
-    counter=0
+    seq=0
     for i in big_imgs:
-        counter+=1
+        seq+=1
         s1=i.get_attribute('src')
         print(i.get_attribute('src'))
-        req = urllib.request.urlretrieve(s1, "big_img_"+str(counter) +'_'+ time.strftime('%m-%d-%I%p-%M-%S', time.localtime(time.time())) + ".jpg")
+        req = urllib.request.urlretrieve(s1, time.strftime('%Y-%m-%d/%Y-%m-%d_naver_%H시_big', time.localtime(time.time()))+str(seq) + ".jpg")
 
     driver.quit()
 
@@ -49,7 +49,7 @@ def mobile_naver_scraping():
         seq=seq+1
         mobile_small_img_link=i.get_attribute('src')
         print(mobile_small_img_link)
-        req = urllib.request.urlretrieve(mobile_small_img_link, time.strftime('%Y-%m-%d_naverM_%H시_', time.localtime(time.time()))+str(seq)+ ".jpg")
+        req = urllib.request.urlretrieve(mobile_small_img_link, time.strftime('%Y-%m-%d/%Y-%m-%d_naverM_%H시_', time.localtime(time.time()))+str(seq)+ ".jpg")
     
     driver.quit()
 
@@ -60,19 +60,36 @@ def youtube_scraping():
     driver.get('https://www.youtube.com/')
     time.sleep(3)
     
-    ad_link=driver.find_element_by_xpath('//*[@id="click-target"]').get_attribute('href')
-    print(ad_link)
+    #ad_link=driver.find_element_by_xpath('//*[@id="click-target"]').get_attribute('href')
+    ad_imgs=driver.find_elements_by_xpath('//*[@id="masthead-ad"]//img')
+    seq=0
+    for imgElement in ad_imgs:
+        seq+=1
+        img=imgElement.get_attribute('src')
+        print(img)
+        req = urllib.request.urlretrieve(img, time.strftime('%Y-%m-%d/%Y-%m-%d_youtube_%H시_', time.localtime(time.time()))+str(seq)+ ".jpg")
+    #print(ad_link)
     
-    driver.get(ad_link)
-    time.sleep(3)
-    ad_subject=driver.find_element_by_xpath('//*[@id="container"]/h1/yt-formatted-string').text
-    print(ad_subject)
-    f=open("youtube_ad_list.txt",'a', encoding='utf8')
-    f.writelines(time.strftime('%Y-%m-%d_youtube_%H시', time.localtime(time.time()))+' '+ad_subject+' LINK : '+ad_link+'\n')
-    f.close()
+    #driver.get(ad_link)
+    #time.sleep(3)
+    #ad_subject=driver.find_element_by_xpath('//*[@id="container"]/h1/yt-formatted-string').text
+    #print(ad_subject)
+    #f=open("youtube_ad_list.txt",'a', encoding='utf8')
+    #f.writelines(time.strftime('%Y-%m-%d_youtube_%H시', time.localtime(time.time()))+' '+ad_subject+' LINK : '+ad_link+'\n')
+    #f.close()
     driver.quit()
 
     threading.Timer(10, youtube_scraping).start()
 
+#폴더생성
+try:
+    if not(os.path.isdir(time.strftime('%Y-%m-%d', time.localtime(time.time())))):
+        os.makedirs(os.path.join(time.strftime('%Y-%m-%d', time.localtime(time.time()))))
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        print("Failed to create directory!!!!!")
+        raise
+
+naver_scraping()
 mobile_naver_scraping()
-                        
+#youtube_scraping() 
